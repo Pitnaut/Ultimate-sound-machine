bdc9454bf6caf078e6a10d1a5c23f007d8e1bbe6
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2024-01-30 11:24:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header className="App-header">
+        <h1>Ultimate Sound Machine</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
