80998fc241fd95ed3242a485fc7a9c7b1d201da2
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2024-01-31 12:49:25 +0100

    Add more sounds

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ function App() {
         <h1>Ultimate Sound Machine</h1>
       </header>
       <Buttons />
+      <footer className="App-footer">
+        <p>Creada por <a href="https://github.com/Pitnaut" target="_blank" rel="noopener noreferrer">Pedro Arévalo</a></p>
+        <p>Código en <a href="https://github.com/Pitnaut/Ultimate-sound-machine" target="_blank" rel="noopener noreferrer"> Github</a></p>
+      </footer>
     </div>
   )
 }
--- src/data/sounds.js
+++ src/data/sounds.js
@@ -31,7 +31,6 @@ import spacejam from '../sounds/spacejam.mp3';
 import bomb from '../sounds/bomb.mp3';
 import teleport from '../sounds/teleport.mp3';
 import dragonball from '../sounds/dragonball.mp3';
-import cyberpunk from '../sounds/cyberpunk.mp3';
 import windows from '../sounds/windows.mp3';
 import hadoken from '../sounds/hadoken.mp3';
 import mushroom from '../sounds/mushroom.mp3';
@@ -45,55 +44,80 @@ import goofy from '../sounds/goofy.mp3';
 import runnin from '../sounds/runnin.mp3';
 import cucaracha from '../sounds/cucaracha.mp3';
 import pistolero from '../sounds/pistolero.mp3';
+import sadtrombone from '../sounds/sadtrombone.mp3';
+import imbecil from '../sounds/imbecil.mp3';
+import celebrate from '../sounds/celebrate.mp3';
+import zeño from '../sounds/zeño.mp3';
+import boing from '../sounds/boing.mp3';
+import bigote from '../sounds/bigote.mp3';
+import fatality from '../sounds/fatality.mp3';
+import getiton from '../sounds/getiton.mp3';
+import drogas from '../sounds/drogas.mp3';
+import herewego from '../sounds/herewego.mp3';
+import dogs from '../sounds/dogs.mp3';
+import bender from '../sounds/bender.mp3';
+import tdfw from '../sounds/tdfw.mp3';
 
 const sounds = [
-  {id: 1, src: airhorn, name: 'airhorn', emoji: '📣'},
-  {id: 2, src: ding, name: 'ding', emoji: '💡'},
-  {id: 3, src: whip, name: 'whip', emoji: '💥'},
-  {id: 4, src: wilhelm, name: 'wilhelm', emoji: '😱'},
-  {id: 5, src: cashier, name: 'cashier', emoji: '💸'},
-  {id: 6, src: foghorn, name: 'foghorn', emoji: '🚢'},
-  {id: 7, src: fart, name: 'fart', emoji: '💨'},
-  {id: 8, src: burp, name: 'burp', emoji: '🤢'},
-  {id: 9, src: snare, name: 'snare', emoji: '🥁'},
-  {id: 10, src: badumtss, name: 'badumtss', emoji: '🎙️'},
-  {id: 11, src: metalgear, name: 'metalgear', emoji: '❗'},
-  {id: 12, src: crowdcheer, name: 'crowdcheer', emoji: '👏'},
-  {id: 13, src: boo, name: 'boo', emoji: '👎'},
-  {id: 14, src: awww, name: 'awww', emoji: '🥰'},
-  {id: 15, src: wooo, name: 'wooo', emoji: '🧐'},
-  {id: 16, src: monkey, name: 'monkey', emoji: '🙈'},
-  {id: 17, src: clownhorn, name: 'clownhorn', emoji: '🤡'},
-  {id: 18, src: robertbweide, name: 'robertbweide', emoji: '🎬'},
-  {id: 19, src: doraemon, name: 'doraemon', emoji: '🐱'},
-  {id: 20, src: johncena, name: 'johncena', emoji: '💪'},
-  {id: 21, src: georgemichael, name: 'georgemichael', emoji: '🎷'},
-  {id: 22, src: guitar, name: 'guitar', emoji: '🎸'},
-  {id: 23, src: chipmunk, name: 'chipmunk', emoji: '🐿️'},
-  {id: 24, src: robocop, name: 'robocop', emoji: '🤖'},
-  {id: 25, src: duffman, name: 'duffman', emoji: '🍺'},
-  {id: 26, src: chiquito, name: 'chiquito', emoji: '👌'},
-  {id: 27, src: nani, name: 'nani', emoji: '❓'},
-  {id: 28, src: angelchoir, name: 'angelchoir', emoji: '👼'},
-  {id: 29, src: snoopdog, name: 'snoopdog', emoji: '🐶'},
-  {id: 30, src: spacejam, name: 'spacejam', emoji: '🏀'},
-  {id: 31, src: bomb, name: 'bomb', emoji: '💣'},
-  {id: 32, src: teleport, name: 'teleport', emoji: '🍃'},
-  {id: 33, src: dragonball, name: 'dragonball', emoji: '🐉'},
-  {id: 34, src: cyberpunk, name: 'cyberpunk', emoji: '🦾'},
-  {id: 35, src: windows, name: 'windows', emoji: '💾'},
-  {id: 36, src: hadoken, name: 'hadoken', emoji: '👊'},
-  {id: 37, src: mushroom, name: 'mushroom', emoji: '🍄'},
-  {id: 38, src: ring, name: 'ring', emoji: '💍'},
-  {id: 39, src: enlab, name: 'enlab', emoji: '⚽'},
-  {id: 40, src: trex, name: 'trex', emoji: '🦖'},
-  {id: 41, src: flute, name: 'flute', emoji: '🎶'},
-  {id: 42, src: mierda, name: 'mierda', emoji: '💩'},
-  {id: 43, src: ivan, name: 'ivan', emoji: '⚔️'},
-  {id: 44, src: goofy, name: 'goofy', emoji: '🤪'},
-  {id: 45, src: runnin, name: 'runnin', emoji: '🏃‍♂️'},
-  {id: 46, src: cucaracha, name: 'cucaracha', emoji: '🚗'},
-  {id: 47, src: pistolero, name: 'pistolero', emoji: '🔫'},
+  {id: 1, src: airhorn, name: 'airhorn', emoji: '📣', categoria: 'sfx'},
+  {id: 2, src: ding, name: 'ding', emoji: '💡', categoria: 'sfx'},
+  {id: 3, src: whip, name: 'whip', emoji: '💥', categoria: 'sfx'},
+  {id: 4, src: wilhelm, name: 'wilhelm', emoji: '😱', categoria: 'sfx'},
+  {id: 5, src: cashier, name: 'cashier', emoji: '💸', categoria: 'sfx'},
+  {id: 6, src: foghorn, name: 'foghorn', emoji: '🚢', categoria: 'sfx'},
+  {id: 7, src: fart, name: 'fart', emoji: '💨', categoria: 'sfx'},
+  {id: 8, src: burp, name: 'burp', emoji: '🤢', categoria: 'sfx'},
+  {id: 9, src: snare, name: 'snare', emoji: '🥁', categoria: 'sfx'},
+  {id: 10, src: badumtss, name: 'badumtss', emoji: '🎙️', categoria: 'sfx'},
+  {id: 11, src: metalgear, name: 'metalgear', emoji: '❗', categoria: 'videojuegos'},
+  {id: 12, src: crowdcheer, name: 'crowdcheer', emoji: '👏', categoria: 'sfx'},
+  {id: 13, src: boo, name: 'boo', emoji: '👎', categoria: 'sfx'},
+  {id: 14, src: awww, name: 'awww', emoji: '🥰', categoria: 'sfx'},
+  {id: 15, src: wooo, name: 'wooo', emoji: '🧐', categoria: 'sfx'},
+  {id: 16, src: monkey, name: 'monkey', emoji: '🙈', categoria: 'sfx'},
+  {id: 17, src: clownhorn, name: 'clownhorn', emoji: '🤡', categoria: 'sfx'},
+  {id: 18, src: robertbweide, name: 'robertbweide', emoji: '🎬', categoria: 'musica'},
+  {id: 19, src: doraemon, name: 'doraemon', emoji: '🐱', categoria: 'musica'},
+  {id: 20, src: johncena, name: 'johncena', emoji: '💪', categoria: 'musica'},
+  {id: 21, src: georgemichael, name: 'georgemichael', emoji: '🎷', categoria: 'musica'},
+  {id: 22, src: guitar, name: 'guitar', emoji: '🎸', categoria: 'musica'},
+  {id: 23, src: chipmunk, name: 'chipmunk', emoji: '🐿️', categoria: 'memes'},
+  {id: 24, src: robocop, name: 'robocop', emoji: '🦾', categoria: 'musica'},
+  {id: 25, src: duffman, name: 'duffman', emoji: '🍺', categoria: 'musica'},
+  {id: 26, src: chiquito, name: 'chiquito', emoji: '👌', categoria: 'memes'},
+  {id: 27, src: nani, name: 'nani', emoji: '❓', categoria: 'memes'},
+  {id: 28, src: angelchoir, name: 'angelchoir', emoji: '👼', categoria: 'sfx'},
+  {id: 29, src: snoopdog, name: 'snoopdog', emoji: '😎', categoria: 'musica'},
+  {id: 30, src: spacejam, name: 'spacejam', emoji: '🏀', categoria: 'musica'},
+  {id: 31, src: bomb, name: 'bomb', emoji: '💣', categoria: 'sfx'},
+  {id: 32, src: teleport, name: 'teleport', emoji: '🍃', categoria: 'sfx'},
+  {id: 33, src: dragonball, name: 'dragonball', emoji: '🐉', categoria: 'musica'},
+  {id: 35, src: windows, name: 'windows', emoji: '💾', categoria: 'memes'},
+  {id: 36, src: hadoken, name: 'hadoken', emoji: '👊', categoria: 'videojuegos'},
+  {id: 37, src: mushroom, name: 'mushroom', emoji: '🍄', categoria: 'videojuegos'},
+  {id: 38, src: ring, name: 'ring', emoji: '💍', categoria: 'videojuegos'},
+  {id: 39, src: enlab, name: 'enlab', emoji: '⚽', categoria: 'memes'},
+  {id: 40, src: trex, name: 'trex', emoji: '🦖', categoria: 'sfx'},
+  {id: 41, src: flute, name: 'flute', emoji: '🎶', categoria: 'memes'},
+  {id: 42, src: mierda, name: 'mierda', emoji: '💩', categoria: 'memes'},
+  {id: 43, src: ivan, name: 'ivan', emoji: '⚔️', categoria: 'memes'},
+  {id: 44, src: goofy, name: 'goofy', emoji: '🤪', categoria: 'sfx'},
+  {id: 45, src: runnin, name: 'runnin', emoji: '🏃‍♂️', categoria: 'sfx'},
+  {id: 46, src: cucaracha, name: 'cucaracha', emoji: '🚗', categoria: 'sfx'},
+  {id: 47, src: pistolero, name: 'pistolero', emoji: '🔫', categoria: 'memes'},
+  {id: 48, src: sadtrombone, name: 'sadtrombone', emoji: '🎺', categoria: 'sfx'},
+  {id: 49, src: imbecil, name: 'imbecil', emoji: '🪓', categoria: 'videojuegos'},
+  {id: 50, src: celebrate, name: 'celebrate', emoji: '🎉', categoria: 'musica'},
+  {id: 51, src: zeño, name: 'zeño', emoji: '🙏', categoria: 'memes'},
+  {id: 52, src: boing, name: 'boing', emoji: '🤸', categoria: 'sfx'},
+  {id: 53, src: bigote, name: 'bigote', emoji: '👨‍🦰', categoria: 'memes'},
+  {id: 54, src: fatality, name: 'fatality', emoji: '☠️', categoria: 'videojuegos'},
+  {id: 55, src: getiton, name: 'getiton', emoji: '🔥', categoria: 'musica'},
+  {id: 56, src: drogas, name: 'drogas', emoji: '💊', categoria: 'memes'},
+  {id: 57, src: herewego, name: 'herewego', emoji: '😤', categoria: 'memes'},
+  {id: 58, src: dogs, name: 'dogs', emoji: '🐶', categoria: 'musica'},
+  {id: 59, src: bender, name: 'bender', emoji: '🤖', categoria: 'memes'},
+  {id: 60, src: tdfw, name: 'tdfw', emoji: '😏', categoria: 'musica'},
 ];
 
 export default sounds;
